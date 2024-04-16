cd4a8569a85ad62831e8017791b0e881c8bd3fe5
generic-name-2166 <kirbesson277@mail.ru>
2024-04-16 14:20:11 +0300

    run

--- src/pyspark_example/main.py
+++ src/pyspark_example/main.py
@@ -1,5 +1,5 @@
 import json
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, DataFrame
 
 
 def load_data() -> dict:
@@ -7,17 +7,34 @@ def load_data() -> dict:
         return json.loads(F.read())
 
 
+def run(
+    products: DataFrame,
+    categories: DataFrame,
+    links: DataFrame,
+) -> DataFrame:
+    join = links.join(products, on="product_id", how="fullouter").withColumnRenamed(
+        "name", "product_name"
+    )
+
+    return (
+        join.join(categories, on="category_id", how="left")
+        .withColumnRenamed("name", "category_name")
+        .select("product_name", "category_name")
+    )
+
+
 def main():
     spark = SparkSession.builder.getOrCreate()
     data = load_data()
-    
-    products = spark.createDataFrame(data["products"])
-    categories = spark.createDataFrame(data["products"])
-    links = spark.createDataFrame(data["links"])
 
-    products.show()
-    categories.show()
-    links.show()
+    products = spark.createDataFrame(data["products"])
+    categories = spark.createDataFrame(data["categories"])
+    links = spark.createDataFrame(data["links"])
+    # products.show()
+    # categories.show()
+    # links.show()
+    result = run(products, categories, links)
+    result.show()
 
 
 if __name__ == "__main__":
